ad45a5ebf4bfa8fa5eee9b06c7f0449e0d9713eb
Felix Stängle <fs156@hdm-stuttgart.de>
2021-11-15 15:51:38 +0100

    HTML Anpassung

--- src/main/java/com/se3/cookingmanager/model/Recipe.java
+++ src/main/java/com/se3/cookingmanager/model/Recipe.java
@@ -11,7 +11,23 @@ public class Recipe implements Serializable {
     private Long id;
     private String name;
     private String ingredient1;
+    private String ingredient2;
+    private String ingredient3;
+    private String ingredient4;
+    private String ingredient5;
+    private String ingredient6;
+    private String ingredient7;
+    private String ingredient8;
+    private String ingredient9;
     private String amount1;
+    private String amount2;
+    private String amount3;
+    private String amount4;
+    private String amount5;
+    private String amount6;
+    private String amount7;
+    private String amount8;
+    private String amount9;
     private String explanation;
     private String imageURL;
     @Column(nullable = false, updatable = false)
@@ -19,10 +35,26 @@ public class Recipe implements Serializable {
 
     public Recipe(){}
 
-    public Recipe(String name, String ingredient1, String amount1, String explanation, String imageUrl, String recipeCode){
+    public Recipe(String name, String ingredient1, String ingredient2, String ingredient3, String ingredient4, String ingredient5, String ingredient6, String ingredient7, String ingredient8, String ingredient9, String amount1, String amount2, String amount3, String amount4, String amount5, String amount6, String amount7, String amount8, String amount9, String explanation, String imageUrl, String recipeCode){
         this.name = name;
         this.ingredient1 = ingredient1;
+        this.ingredient2 = ingredient2;
+        this.ingredient3 = ingredient3;
+        this.ingredient4 = ingredient4;
+        this.ingredient5 = ingredient5;
+        this.ingredient6 = ingredient6;
+        this.ingredient7 = ingredient7;
+        this.ingredient8 = ingredient8;
+        this.ingredient9 = ingredient9;
         this.amount1 = amount1;
+        this.amount1 = amount2;
+        this.amount1 = amount3;
+        this.amount1 = amount4;
+        this.amount1 = amount5;
+        this.amount1 = amount6;
+        this.amount1 = amount7;
+        this.amount1 = amount8;
+        this.amount1 = amount9;
         this.explanation = explanation;
         this.imageURL = imageUrl;
         this.recipeCode = recipeCode;
@@ -60,6 +92,134 @@ public class Recipe implements Serializable {
         this.amount1 = jobTitle;
     }
 
+    public String getIngredient2() {
+        return ingredient2;
+    }
+
+    public void setIngredient2(String ingredient2) {
+        this.ingredient2 = ingredient2;
+    }
+
+    public String getIngredient3() {
+        return ingredient3;
+    }
+
+    public void setIngredient3(String ingredient3) {
+        this.ingredient3 = ingredient3;
+    }
+
+    public String getIngredient4() {
+        return ingredient4;
+    }
+
+    public void setIngredient4(String ingredient4) {
+        this.ingredient4 = ingredient4;
+    }
+
+    public String getIngredient5() {
+        return ingredient5;
+    }
+
+    public void setIngredient5(String ingredient5) {
+        this.ingredient5 = ingredient5;
+    }
+
+    public String getIngredient6() {
+        return ingredient6;
+    }
+
+    public void setIngredient6(String ingredient6) {
+        this.ingredient6 = ingredient6;
+    }
+
+    public String getIngredient7() {
+        return ingredient7;
+    }
+
+    public void setIngredient7(String ingredient7) {
+        this.ingredient7 = ingredient7;
+    }
+
+    public String getIngredient8() {
+        return ingredient8;
+    }
+
+    public void setIngredient8(String ingredient8) {
+        this.ingredient8 = ingredient8;
+    }
+
+    public String getIngredient9() {
+        return ingredient9;
+    }
+
+    public void setIngredient9(String ingredient9) {
+        this.ingredient9 = ingredient9;
+    }
+
+    public String getAmount2() {
+        return amount2;
+    }
+
+    public void setAmount2(String amount2) {
+        this.amount2 = amount2;
+    }
+
+    public String getAmount3() {
+        return amount3;
+    }
+
+    public void setAmount3(String amount3) {
+        this.amount3 = amount3;
+    }
+
+    public String getAmount4() {
+        return amount4;
+    }
+
+    public void setAmount4(String amount4) {
+        this.amount4 = amount4;
+    }
+
+    public String getAmount5() {
+        return amount5;
+    }
+
+    public void setAmount5(String amount5) {
+        this.amount5 = amount5;
+    }
+
+    public String getAmount6() {
+        return amount6;
+    }
+
+    public void setAmount6(String amount6) {
+        this.amount6 = amount6;
+    }
+
+    public String getAmount7() {
+        return amount7;
+    }
+
+    public void setAmount7(String amount7) {
+        this.amount7 = amount7;
+    }
+
+    public String getAmount8() {
+        return amount8;
+    }
+
+    public void setAmount8(String amount8) {
+        this.amount8 = amount8;
+    }
+
+    public String getAmount9() {
+        return amount9;
+    }
+
+    public void setAmount9(String amount9) {
+        this.amount9 = amount9;
+    }
+
     public String getExplanation() {
         return explanation;
     }
@@ -89,9 +249,25 @@ public class Recipe implements Serializable {
         return "Recipe{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", email='" + ingredient1 + '\'' +
-                ", jobTitle='" + amount1 + '\'' +
-                ", phone='" + explanation + '\'' +
+                ", ingredient1='" + ingredient1 + '\'' +
+                ", ingredient2='" + ingredient2 + '\'' +
+                ", ingredient3='" + ingredient3 + '\'' +
+                ", ingredient4='" + ingredient4 + '\'' +
+                ", ingredient5='" + ingredient5 + '\'' +
+                ", ingredient6='" + ingredient6 + '\'' +
+                ", ingredient7='" + ingredient7 + '\'' +
+                ", ingredient8='" + ingredient8 + '\'' +
+                ", ingredient9='" + ingredient9 + '\'' +
+                ", amount1='" + amount1 + '\'' +
+                ", amount2='" + amount2 + '\'' +
+                ", amount3='" + amount3 + '\'' +
+                ", amount4='" + amount4 + '\'' +
+                ", amount5='" + amount5 + '\'' +
+                ", amount6='" + amount6 + '\'' +
+                ", amount7='" + amount7 + '\'' +
+                ", amount8='" + amount8 + '\'' +
+                ", amount9='" + amount9 + '\'' +
+                ", explanation='" + explanation + '\'' +
                 ", imageUrl='" + imageURL + '\'' +
                 '}';
     }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,13 +4,29 @@ CREATE TABLE recipe (
     id INT PRIMARY KEY,
     name VARCHAR(250),
     ingredient1 VARCHAR(250),
+    ingredient2 VARCHAR (250),
+    ingredient3 VARCHAR (250),
+    ingredient4 VARCHAR (250),
+    ingredient5 VARCHAR (250),
+    ingredient6 VARCHAR (250),
+    ingredient7 VARCHAR (250),
+    ingredient8 VARCHAR (250),
+    ingredient9 VARCHAR (250),
     amount1 VARCHAR(250),
+    amount2 VARCHAR(250),
+    amount3 VARCHAR(250),
+    amount4 VARCHAR(250),
+    amount5 VARCHAR(250),
+    amount6 VARCHAR(250),
+    amount7 VARCHAR(250),
+    amount8 VARCHAR(250),
+    amount9 VARCHAR(250),
     explanation VARCHAR(250),
     imageURL VARCHAR(250),
     recipe_Code VARCHAR(250)
 );
 
-INSERT INTO recipe (id, name, ingredient1, amount1, explanation, imageUrl) VALUES
-('1', 'Spaghetti', 'Spaghetti', '1 Pck.', 'Einfach Spaghetti in Topf du Penner', 'https://image.brigitte.de/12510166/t/f9/v3/w960/r1/-/spaghetti-alla-puttanesca.jpg'),
-('2', 'Burger', 'Fleisch', '10 kg', 'Drauf fetzen. Fertig.', 'https://www.edeka.de/media/01-rezeptbilder/rezeptbilder-a-d/rez-edeka-burger-the-big-american-rezept-a-d-1-1.jpg'),
-('3', 'Dönerteller', 'Knoblauch-Sauce', '1 Flasche', 'Egal wie gut, Ferhat seiner ist beste.', 'https://img.restaurantguru.com/caf2-Restaurant-Ferhat-Doner-Grill-interior.jpg');
+INSERT INTO recipe (id, name, ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6, ingredient7, ingredient8, ingredient9, amount1, amount2, amount3, amount4, amount5, amount6, amount7, amount8, amount9, explanation, imageUrl) VALUES
+('1', 'Spaghetti', 'Spaghetti', null, null, null, null, null, null, null, null, '1 Pck.', null, null, null, null, null, null, null, null, 'Einfach Spaghetti in Topf du Penner', 'https://image.brigitte.de/12510166/t/f9/v3/w960/r1/-/spaghetti-alla-puttanesca.jpg'),
+('2', 'Burger', 'Fleisch', null, null, null, null, null, null, null, null, '10 kg', null, null, null, null, null, null, null, null, 'Drauf fetzen. Fertig.', 'https://www.edeka.de/media/01-rezeptbilder/rezeptbilder-a-d/rez-edeka-burger-the-big-american-rezept-a-d-1-1.jpg'),
+('3', 'Dönerteller', 'Knoblauch-Sauce', null, null, null, null, null, null, null, null, '1 Flasche', null, null, null, null, null, null, null, null, 'Egal wie gut, Ferhat seiner ist beste.', 'https://img.restaurantguru.com/caf2-Restaurant-Ferhat-Doner-Grill-interior.jpg');
